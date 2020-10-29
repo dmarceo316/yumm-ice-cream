871b46003830160d0198b8e81b6570553918e7a7
dmarceo316 <arceo.adam@gmail.com>
2020-10-29 16:53:06 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const iceDogLink = document.querySelector(".icedog a");
+const pikePlaceLink = document.querySelector(".pikeplace a");
+const iceCreamLink = document.querySelector(".ice-cream-1 a");
+const conesLink = document.querySelector(".cones a")
+const socialMediaLink = document.querySelector(".social-media a")
+
+
+// Will display and hide icedog link on ice dog
+function displayIceDog(){
+    iceDogLink.style.display = "block";
+}
+function hideIceDog(){
+    iceDogLink.style.display = "none";
+}
+
+// Will display and hide pikeplace link
+function displayPike(){
+    pikePlaceLink.style.display = "block";
+}
+function hidePike(){
+    pikePlaceLink.style.display = "none";
+}
+
+// will display and hide  icecream link
+function displayIceCream(){
+    iceCreamLink.style.display = "block";
+}
+function hideIceCream(){
+    iceCreamLink.style.display = "none";
+}
+
+// Will display and hide cones link
+function displayCones(){
+    conesLink.style.display = "block";
+}
+function hideCones(){
+    conesLink.style.display = "none";
+}
+
+// Will display and hide social media links
+function displaySocialMedia(){
+    socialMediaLink.style.display= "block";
+}
+function hideSocialMedia(){
+    socialMediaLink.style.display = "none";
+}
+
+
